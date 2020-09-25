aef591af42402430111e69925b2e8ae118c49c30
vivian47 <64009526+vivian47@users.noreply.github.com>
2020-09-25 15:47:23 +0800

    Fixes (#24)
    * Fixes
    
    * Update script.js
    
    * Delete script.js

--- server/static/script.js
+++ server/static/script.js
@@ -1,7 +1,7 @@
 function processFiles() {
     $.get("/result", function(data, status){
-		document.getElementById("p1").innerHTML = data;
-		document.getElementById("p2").innerHTML = '<a href="/getResults" download><button>Download</button></a>';
+		document.getElementById("output").innerHTML = data;
+		document.getElementById("results").innerHTML = '<a href="/getResults" download><button>Download</button></a>';
 	});
 }
 
@@ -12,28 +12,3 @@ function copyToClipboard(element) {
     document.execCommand("copy");
     $temp.remove();
 }
-
-Dropzone.autoDiscover = false;
-
-var myDropzone = new Dropzone(".dropzone", {
-  addRemoveLinks: true,
-  removedfile: function(file) {
-    var fileName = file.name;
-
-    $.ajax({
-      type: 'POST',
-      url: '../static/upload.php',
-      data: {name: fileName,request: 'delete'},
-    });
-
-    var _ref;
-    return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
-   }
-});
-
-function removeFunction() {
-    Dropzone.forElement(".dropzone").removeAllFiles(true);
-};
-
-var el = document.getElementById("classify");
-el.addEventListener("click", removeFunction);  

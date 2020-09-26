349a8a044b2302af0e309ac749bb065e23aa7d84
og1764 <21958054@student.uwa.edu.au>
2020-09-26 15:43:22 +0800

    moved to script.js

--- server/static/script.js
+++ server/static/script.js
@@ -1,11 +1,3 @@
-function processFiles() {
-	//Dropzone.forElement('.dropzone').processQueue();
-    $.get("/result", function(data, status){
-		document.getElementById("output").innerHTML = data;
-		document.getElementById("results").innerHTML = '<a href="/getResults" download><button>Download</button></a>';
-	});
-}
-
 function copyToClipboard(element) {
     var $temp = $("<input>");
     $("body").append($temp);
@@ -13,3 +5,26 @@ function copyToClipboard(element) {
     document.execCommand("copy");
     $temp.remove();
 }
+
+Dropzone.autoDiscover = false;
+	
+var myDropzone = new Dropzone(".dropzone", {
+  //addRemoveLinks: true,
+  autoProcessQueue: false,
+  addRemoveLinks: true,
+  maxFiles: 10,
+  parallelUploads: 10,
+  uploadMultiple: true,
+  url: "/upload",
+  init: function() {
+	this.on("successmultiple", function(data, status) {
+		//processFiles();
+		document.getElementById("output").innerHTML = status;
+		document.getElementById("results").innerHTML = '<a href="/getResults" download><button>Download</button></a>';
+		this.removeAllFiles()
+	});
+  },
+});
+
+//var el = document.getElementById("classify");
+//el.addEventListener("click", removeFunction);

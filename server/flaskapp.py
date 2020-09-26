4cdd535125b8679d508c564faae13d49b28d6c44
Abdi-Isse <abdi.isse96@gmail.com>
2020-09-26 23:31:42 +0800

    change back port name

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -34,8 +34,6 @@ import datetime
 import random
 import string
 
-port = int(os.environ.get('PORT', 33507))
-
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 VALID_COMPRESSED = (".zip", ".tar.gz", ".tar") # TODO: Add 7z, Add rar,
 # tuple so it can be used with .endswith
@@ -342,5 +340,5 @@ def upload():
     # print(output)
     return process_images(target)
 
-
-app.run(debug=True, port=port)
+if __name__ == "__main__":
+    app.run()

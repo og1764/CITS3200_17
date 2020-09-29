e35c42b947a4b476282602b09dc865c67f479b70
og1764 <21958054@student.uwa.edu.au>
2020-09-29 23:58:29 +0800

    have I actually done this

--- server/app.py
+++ server/app.py
@@ -1,39 +1,47 @@
-import datetime
-import glob
-import keras
-import keras.callbacks
-import numpy as np
-import os
-import os.path
-import random
-import requests
-import shutil
-import string
-import sys
-import tarfile
-import time
-import urllib.request
-import zipfile
+from config import Config
 from Form import LoginForm
 from Model import query_user, User
-from PIL import Image, UnidentifiedImageError
-from bs4 import BeautifulSoup
-from config import Config
-from flask import flash, Flask, json, redirect, render_template, request, Response, send_file, send_from_directory, url_for
-from flask_login import current_user, login_required, login_user, LoginManager, logout_user
+from flask import Flask, render_template, request, json
+from flask import render_template, flash, redirect, url_for, request, send_from_directory, send_file, Response
+from flask_login import logout_user, login_user, current_user, login_required, LoginManager
+from werkzeug.urls import url_parse
 from flask_wtf import FlaskForm
-from keras import backend as K
+from wtforms import SelectField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
+
+import keras
 from keras.datasets import mnist
-from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
-from keras.models import model_from_json, Sequential
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 from keras.utils import np_utils
+from keras.models import model_from_json
+import keras.callbacks
+import numpy as np
+import os.path
+import os
+import shutil
 from os import listdir
 from os.path import isfile, join
-from stat import S_ISREG, ST_CTIME, ST_MODE
-from werkzeug.urls import url_parse
-from wtforms import SelectField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+import glob
+from PIL import Image, UnidentifiedImageError
+import sys
 
+import tarfile
+import zipfile
+import datetime
+import random
+import string
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import multiprocessing
+from multiprocessing import Pool
+
+import time
+import requests
+from bs4 import BeautifulSoup
+from stat import S_ISREG, ST_CTIME, ST_MODE
+import urllib.request
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 VALID_COMPRESSED = (".zip", ".tar.gz", ".tar")  # TODO: Add 7z, Add rar,
@@ -47,6 +55,10 @@ app.secret_key = 'SECRET KEY'
 login = LoginManager(app)
 login.login_view = 'login'
 
+# if os.name == "nt":
+#    sh = "\\"
+# else:
+#    sh = "/"
 sh = os.sep
 
 img_types = ['.jpg', '.png']
@@ -86,6 +98,7 @@ def process_compressed(compressed_list):
 
 # target = FULL PATH of a folder
 def files_in_folder(target):
+
     file_list = os.listdir(target)
     result = []
     # Iterate over all the entries
@@ -95,19 +108,17 @@ def files_in_folder(target):
         if os.path.isdir(path):
             result = result + files_in_folder(path)
         elif not path.endswith(VALID_COMPRESSED):
-            #print(path)
+            print(path)
             result.append(path)
     return result
 
 # files = array of images
-def normalise_images(files, target):
+def normalise_images(files):
     # classifies all files, gives an error if not a valid image type.
+    image_values = []
     for file in files:
-        #name = " " + file.split(sh)[-1]
-        name = file.replace(target, "")
-        name = name.replace(".dir", "")
+        name = " " + file.split(sh)[-1]
         print(name)
-        #print(name)
         try:
             initial = Image.open(file)
             result = initial.resize((50, 50)).convert("L")
@@ -140,7 +151,7 @@ def bulk_classify(files, loaded_model, ID, GLOBAL_FOLDER_DICT):
             return_values.append((CNN(i[0], loaded_model, counter), i[1]))
         else:
             return_values.append((i[0], ""))
-        # Potentially remove this if we don't want to keep a progress text file
+        # rand_folder =
         f = open(GLOBAL_FOLDER_DICT[ID][0], "w")
         f.write("S: " + "NOT_COMPLETE\n")
         f.write("T: " + str(number_files) + "\n")
@@ -174,6 +185,8 @@ def file_cleanup(target, compressed, folders):
 # results = results from classification
 def format_results(ID, results, GLOBAL_FOLDER_DICT):
     ret = ""
+    print(ID)
+    print(results)
     f = open(GLOBAL_FOLDER_DICT[ID][1], "w+")
     for i in results:
         for j in i:
@@ -182,6 +195,15 @@ def format_results(ID, results, GLOBAL_FOLDER_DICT):
     f.close()
     if ret == "":
         ret = "Example text"
+    else:  # If something was actually classified
+        # This is all for the returnFile() function. We need to figure out how to use identifiers though
+        # Because it's a bit of a jank-fest atm, and it really only supports one user.
+        # This one needs to be fixed
+        f = open(APP_ROOT + sh + "toSend" + sh + "ThisFileWillAlwaysHaveThisNameAndThatsBad.txt", "w")
+        toSend = ret.replace("<br>", "\n")
+        f.write(toSend)
+        f.close()
+
     return ret
 
 
@@ -320,8 +342,21 @@ def CNN(lines, loaded_model, number):
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
+    # load json and create model
+    # json_file = open(APP_ROOT + sh + 'ct3200.dir' + sh + 'model.json', 'r')
+    # loaded_model_json = json_file.read()
+    # json_file.close()
+    # loaded_model = model_from_json(loaded_model_json)
+
+    # load weights into new model
+    # loaded_model.load_weights(APP_ROOT + sh + "ct3200.dir" + sh + "model.h5")
+    # print("Loaded model from disk")
+
     y_vec = np.zeros(num_classes)
     y_pred = loaded_model.predict(x_test)
+    # print(y_pred[:ntest])
+
+    # return_values.append(str(ntest)+" || ")
 
     for i in range(ntest):
         for j in range(num_classes):
@@ -329,7 +364,12 @@ def CNN(lines, loaded_model, number):
         y_type = np.argmax(y_vec)
         prob = y_vec[y_type]
 
-        out_str = '  i=' + str(i) + '  \nG-type=' + str(y_type) + '  P=' + str(prob)
+        # print('i=', i, 'G-type=', y_type, 'P', prob)
+        #  Original  type-1 is output
+        # out_str = str(y_type) + ', ' + str(y_vec[0]) + ', '+ str(y_vec[1]) + ', ' + str(y_vec[2]) + "\n"
+        out_str = '  i=' + str(i) + '  \nG-type=' + \
+                  str(y_type) + '  P=' + str(prob)
+        # return_values.append(out_str)
         formatted_prob = prob * 100
         if y_type == 0:
             return_values.append("E - {0:.2f}% -".format(formatted_prob))
@@ -418,10 +458,10 @@ def main():
 
 
 # This is more than a little jank. We're going to have to figure out how to use identifiers here as well
-@app.route('/getResults/<token>')
-def returnFile(token):
+@app.route('/getResults')
+def returnFile():
     print("We're here!")
-    with open(GLOBAL_FOLDER_DICT[1]) as f:
+    with open(APP_ROOT + sh + "toSend" + sh + "ThisFileWillAlwaysHaveThisNameAndThatsBad.txt") as f:
         txt_content = f.read()
     toReturn = Response(
         txt_content,
@@ -437,31 +477,46 @@ def logout():
     return redirect(url_for('login'))
 
 
+# Below is an example of how to add other pages
+# To go to this webpage you would need to add "/Examplepage" to the end of the main page url
+
+
+# @app.route("/result", methods=['GET', 'POST'])
 # @app.route("/result")
-# target = FULL PATH to folder
-def process_images(target, GLOBAL_FOLDER_DICT):
-    
+def OLD_process_images(target):
     print("started")
     print(datetime.datetime.now())
     t1 = datetime.datetime.now()
-    
-    rand_identifier = target.split(sh)[-2]
+    rand_folder = target.split(sh)[-2]
+    print(rand_folder)
+    image_values = []
+    return_values = []
+    folder_list = []
     uploads_path = target
-    results_path = GLOBAL_FOLDER_DICT[rand_identifier][1]
+    compressed_list = [uploads_path + sh + filename for filename in os.listdir(uploads_path) if
+                       filename.endswith(VALID_COMPRESSED)]
+    print(compressed_list)
 
-    compressed_list = [target + sh + filename for filename in os.listdir(target) if filename.endswith(VALID_COMPRESSED)]
-
-    print(datetime.datetime.now())
-    
     # Loop over and extract compressed folders
-    
-    folder_list = process_compressed(compressed_list)
+    for file in compressed_list:
+        if file.endswith((".tar", ".tar.gz")):
+            tf = tarfile.open(file)
+            os.mkdir(file + ".dir" + sh)
+            folder_list.append(file + ".dir" + sh)
+            tf.extractall(file + ".dir" + sh)
+            tf.close()
+        if file.endswith(".zip"):
+            with zipfile.ZipFile(file, 'r') as zip:
+                os.mkdir(file + ".dir" + sh)
+                folder_list.append(file + ".dir" + sh)
+                zip.extractall(file + ".dir" + sh)
 
     print("extracting done")
     print(datetime.datetime.now())
+    t2 = datetime.datetime.now()
 
     # Check out my onlyfiles ;)
-    onlyfiles = files_in_folder(target)
+    onlyfiles = files_in_folder(uploads_path)
     print(onlyfiles)
 
     # Moved this out of the CNN function because its expensive, so its better to only call once.
@@ -471,23 +526,152 @@ def process_images(target, GLOBAL_FOLDER_DICT):
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
     loaded_model.load_weights(APP_ROOT + sh + "ct3200.dir" + sh + "model.h5")
-    
+
+    # classifies all files, gives an error if not a valid image type.
+    for file in onlyfiles:
+        name = " " + file.split(sh)[-1]
+        print(name)
+        try:
+            initial = Image.open(file)
+            result = initial.resize((50, 50)).convert("L")
+            pix_val = list(result.getdata())
+            norm_val = [i / 255 for i in pix_val]
+            image_values.append((norm_val, name + " <br>"))
+            initial.close()
+        except UnidentifiedImageError:
+            print("Unidentified Image Error")
+            extension = name.split(".")[-1]
+            error_message = 'Could not classify "<b>{}</b>" as <b>.{}</b> is not a valid file extension. <br>'.format(
+                name.lstrip(), extension.rstrip())
+            image_values.append((error_message, ""))
+        except:
+            print(str(sys.exc_info()[1]) + " @ Line " + str(sys.exc_info()[2].tb_lineno))
+        os.remove(file)
+
+    # counter is not really useful, but it's nice to see the console doing things.
+    counter = 0
+    number_files = len(image_values)
+    for i in image_values:
+        # print(i)
+        counter = counter + 1
+        if i[1] != "":
+            return_values.append((CNN(i[0], loaded_model, counter), i[1]))
+            # POST to folder name with progress
+        else:
+            return_values.append((error_message, ""))
+
+    # removes compressed files after they've been extracted.
+    # this isnt in the above loop because I was getting weird permission errors where the zip was still in use
+    # Even after moving it all the way down here it still thinks its in use >:(
+    #
+    # This was the best solution I could come up with. We're going to get a lot of errors on this first try-catch but if it works...
+    try:
+        tf.close()
+    except:
+        print(str(sys.exc_info()[1]) + " @ Line " + str(sys.exc_info()[2].tb_lineno))
+    for file in compressed_list:
+        try:
+            os.remove(file)
+        except:
+            print(str(sys.exc_info()[1]) + " @ Line " + str(sys.exc_info()[2].tb_lineno))
+
+    # Removing the empty zipped folders.
+    for folder in folder_list:
+        shutil.rmtree(folder)
+    shutil.rmtree(target)
+    print(datetime.datetime.now())
+    t4 = datetime.datetime.now()
+    tot_time = t4 - t1
+    print("\nTime taken for this request: " + str(tot_time) + "\n")
+
+    # Just a bit of formatting
+    ret = ""
+    for i in return_values:
+        for j in i:
+            ret = ret + "".join(j).replace(target, "")
+    if ret == "":
+        ret = "Example text"
+    else:  # If something was actually classified
+        # This is all for the returnFile() function. We need to figure out how to use identifiers though
+        # Because it's a bit of a jank-fest atm, and it really only supports one user.
+        f = open(APP_ROOT + sh + "toSend" + sh + "ThisFileWillAlwaysHaveThisNameAndThatsBad.txt", "w")
+        toSend = ret.replace("<br>", "\n")
+        f.write(toSend)
+        f.close()
+    print(ret)
+    # POST to folder name
+
+    dictToSend = {'Total': number_files, 'Complete': counter, 'Results': ret}
+    res = requests.post('http://localhost:5000/get_file/' + rand_folder, json=dictToSend)
+    print
+    'response from server:', res.text
+    dictFromServer = res.json()
+
+    return ret
+
+
+# @app.route("/result")
+# target = FULL PATH to folder
+def process_images(target, GLOBAL_FOLDER_DICT):
+    print("started")
+    f = open(APP_ROOT + sh + "toSend" + sh + "NEWNAMETEST.txt", "w")
+    f.write("hehexd")
+    f.close()
+    print(datetime.datetime.now())
+    #sys.stdout.flush()
+    t1 = datetime.datetime.now()
+    rand_identifier = target.split(sh)[-2]
+    #sys.stdout.flush()
+    uploads_path = target
+    results_path = GLOBAL_FOLDER_DICT[rand_identifier][1]
+
+    compressed_list = [target + sh + filename for filename in os.listdir(target) if filename.endswith(VALID_COMPRESSED)]
+
+    print(datetime.datetime.now())
+    # Loop over and extract compressed folders
+    folder_list = process_compressed(compressed_list)
+    f = open(APP_ROOT + sh + "toSend" + sh + "COMPRESSED.txt", "w")
+    f.write("hehexd")
+    f.close()
+
+    print("extracting done")
+    print(datetime.datetime.now())
+    #sys.stdout.flush()
+
+    # Check out my onlyfiles ;)
+    onlyfiles = files_in_folder(target)
+    print(onlyfiles)
+    #sys.stdout.flush()
+
+    # Moved this out of the CNN function because its expensive, so its better to only call once.
+    json_file = open(APP_ROOT + sh + 'ct3200.dir' + sh + 'model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(APP_ROOT + sh + "ct3200.dir" + sh + "model.h5")
+
     # normalises image files, gives an error if not a valid image type.
-    image_values = normalise_images(onlyfiles, target)
+    image_values = normalise_images(onlyfiles)
+
+    f = open(APP_ROOT + sh + "toSend" + sh + "NORM.txt", "w")
+    f.write("hehexd")
+    f.close()
 
     # sends normalised images into the classifier
+    # IN BULK CLASSIFY WE WANT TO:
+    # WRITE PROGRESS TO FILE
     return_values = bulk_classify(image_values, loaded_model, rand_identifier, GLOBAL_FOLDER_DICT)
 
     # removes compressed files and folders after they've been extracted.
     file_cleanup(target, compressed_list, folder_list)
 
-    # This formats results to be sent back and creates a text file
     to_send = format_results(rand_identifier, return_values, GLOBAL_FOLDER_DICT)
     # to_send is an array
 
     ''' CREATE FILE '''
     print(rand_identifier)
-    # this updates the progress.txt file
+
     f = open(GLOBAL_FOLDER_DICT[rand_identifier][0], "w")
     f.write("S: " + "CLASSIFICATION_COMPLETE\n")
     f.write("T: " + str(len(onlyfiles)) + "\n")
@@ -502,6 +686,42 @@ def process_images(target, GLOBAL_FOLDER_DICT):
 
     return to_send
 
+    # POST to folder name
+
+    # dictToSend = {'Total': number_files, 'Complete': counter, 'Results': ret}
+    # res = requests.post('http://localhost:5000/get_file/' + rand_folder, json=dictToSend)
+    # print('response from server: ' + str(res.text))
+    # dictFromServer = res.json()
+
+
+# @app.route("/upload", methods=['POST'])
+def OLD_upload():
+    up_folder = str(APP_ROOT) + sh + "uploads" + sh
+    if not os.path.exists(up_folder):
+        os.mkdir(up_folder)
+    rand_folder = ''.join(random.choice(string.ascii_letters) for i in range(12))
+    target = str(APP_ROOT) + sh + "uploads" + sh + rand_folder + sh
+    print(target)
+    while os.path.exists(target):
+        rand_folder = ''.join(random.choice(string.ascii_letters) for i in range(12))
+        target = str(APP_ROOT) + sh + "uploads" + sh + rand_folder + sh
+
+    os.mkdir(target)
+
+    for file in request.files.values():
+        filename = file.filename
+        destination = sh.join([target, filename])
+        file.save(destination)
+
+    # output = process_images()
+    # print(output)
+
+    s = Serializer('WEBSITE_SECRET_KEY', 60 * 60 * 24)  # 60 secs by 60 mins by 24 hours
+    token = s.dumps({'user_id': rand_folder}).decode('utf-8')  # encode user id
+
+    # Make a POST request with the folder name
+
+    return process_images(target)
 
 
 @app.route("/upload", methods=['POST'])
@@ -526,7 +746,9 @@ def upload():
 
     os.mkdir(up_target)
     os.mkdir(res_target)
-
+    print(res_target)
+    print(os.path.exists(res_target))
+    print("fuck me")
     rand_progress = res_target + "progress.txt"
     f = open(rand_progress, "w")
     f.write("S: " + "NOT_COMPLETE\n")
@@ -545,20 +767,27 @@ def upload():
         destination = sh.join([up_target, filename])
         file.save(destination)
 
+    # output = process_images()
+    # print(output)
     time_now = datetime.datetime.now(datetime.timezone.utc)
     LIFETIME = datetime.timedelta(days=1)
     expiry = time_now + LIFETIME
 
     GLOBAL_FOLDER_DICT[rand_identifier] = [rand_progress, rand_results, up_target, res_target, expiry]
     print(rand_identifier)
+    ''' START SUBPROCESS '''
+    ''' process_images(rand_identifier) '''
+    #thread = multiprocessing.Process(target=process_images, args=(GLOBAL_FOLDER_DICT[rand_identifier][2],))
+    #thread.start()
+    f = _pool.apply_async(process_images,[GLOBAL_FOLDER_DICT[rand_identifier][2], GLOBAL_FOLDER_DICT])
+    r = f.get(timeout=2)
+    print(r)
+    #return 'Result is %d'%r
+
+    #process_images(GLOBAL_FOLDER_DICT[rand_identifier][2])
 
     # 202 Accepted
     return rand_identifier, 202
-
-
-@app.route('/start/<token>', methods=["GET"])
-def start_processing(token):
-    return process_images(GLOBAL_FOLDER_DICT[token][2], GLOBAL_FOLDER_DICT)
 
 
 @app.route('/results/<token>', methods=["GET"])
@@ -591,4 +820,7 @@ def check_progress(token):
 
 
 if __name__ == "__main__":
+    _pool = Pool(processes=4)
+    manager = multiprocessing.Manager()
+    GLOBAL_FOLDER_DICT = manager.dict()
     app.run()

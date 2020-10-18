666b889dd5898f0095743ac181c0671875c61da3
og1764 <21958054@student.uwa.edu.au>
2020-10-19 00:54:51 +0800

    smol updates

--- server/Model.py
+++ server/Model.py
@@ -3,7 +3,11 @@ from flask_login import UserMixin
 class User(UserMixin):
     pass
 
-users = [{'id' : 'admin', 'username': 'admin', 'password': 'password'}]
+users = [{'id' : 'admin', 
+    'username': 'admin', 
+    'password': 'password'
+    }]
+    
 def query_user(user_id):
     for user in users:
         if user_id == user['id']:
